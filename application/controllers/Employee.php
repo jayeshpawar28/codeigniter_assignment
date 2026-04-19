242e66772be0e603cf20120acefee78b5a2b035a
Jayesh <jayesh281103@gmail.com>
2026-04-19 23:28:05 +0530

    Updated Employee controller

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Employee extends CI_Controller {
 
+    public function test(){
+        echo 'he';
+    }
+
     public function __construct() {
         parent::__construct();
         if (!$this->session->userdata('logged_in') || $this->session->userdata('user_type') != 'employee') {
